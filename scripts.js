8764e46f6cfae84de3d4a7ec5409f02e9cd3e9cc
1-rr-pbrf <yume111168275@gmail.com>
2024-12-09 03:38:10 +0900

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,23 @@
+document.addEventListener('DOMContentLoaded', function () {
+  const dotArts = document.querySelectorAll('.dot-art');
+
+  dotArts.forEach(function(dotArt) {
+      const staticImage = dotArt.querySelector('.static');
+      const animatedImage = dotArt.querySelector('.animated');
+
+      dotArt.addEventListener('mouseover', function () {
+          staticImage.style.display = 'none';
+          animatedImage.style.display = 'block';
+      });
+
+      dotArt.addEventListener('mouseout', function () {
+          staticImage.style.display = 'block';
+          animatedImage.style.display = 'none';
+          
+          // GIFのループを最初から再生するためにソースをリセット
+          const src = animatedImage.src;
+          animatedImage.src = '';
+          animatedImage.src = src;
+      });
+  });
+});
